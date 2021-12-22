0d38e4062c4de104bf573ba6bf5f5966fc855dce
Ludine <ludskieverde@gmail.com>
2021-12-22 22:11:12 +0800

    created filter for place and salary offer

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from bs4 import BeautifulSoup
 import requests, re
 
+far = ['NATIONAL', 'CEBU','ILOILO']
+min_salary_k = 40
+
+def filter(targ_sal, place, offer) :
+    dist_ok = True
+    sal_ok = True
+    for i in range(len(far)):
+                if far[i] in place.upper():
+                    print(far[i], place)
+                    dist_ok = False
+                    break
+    
+    if offer == 'none':
+        print('none')
+    elif offer < targ_sal:
+        print(offer, 'bad')
+        sal_ok = False
+    elif offer >= targ_sal:
+        print(offer, 'good')
+    
+    if sal_ok == True and dist_ok ==True:
+        print('OK')
+
+    print()
+
 url = 'https://www.jobstreet.com.ph/en/job-search/computer-software-it-jobs/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
 # your version here: chrome://settings/help
@@ -21,7 +46,7 @@ for job in jobs:
 
     if 'h' in time or ('d' in time and t < 12):
 
-        # find inside job
+        # find job title
         title = job.find('div', class_='sx2jih0 _2j8fZ_0 sIMFL_0 _1JtWu_0').text.replace(' ','')
         # find company
         com = job.find('span', class_='sx2jih0 zcydq82q _18qlyvc0 _18qlyvcv _18qlyvc1 _18qlyvc8').text
@@ -31,31 +56,26 @@ for job in jobs:
         pay = job.find_all('span', class_='sx2jih0 zcydq82q _18qlyvc0 _18qlyvcv _18qlyvc3 _18qlyvc6')
         if len(pay) < 2:
             pay = 'none'
-        else: pay = pay[1].text
-        # find link
-        href = job.div.h1.a['href']
-        if site not in href:
-            link = site + href
-        else:
-            link = href
+        else: 
+            pay = re.search('\d+', pay[1].text)
+            pay = int(pay.group(0))
 
-        print(f'''
-JOB TITLE: {title}
-LOCATION: {com}, {place}   DATE: {time}     
-PAY: {pay}
+        filter(min_salary_k, place, pay)
 
-link: {link}   \n''')
+#         if match == True:
+                  
+        
+#             # find link
+#             href = job.div.h1.a['href']
+#             if site not in href:
+#                 link = site + href
+#             else:
+#                 link = href
 
+#             print(f'''-------------
+# JOB TITLE: {title}
+# LOCATION: {com} | {place}   DATE: {time}     
+# PAY: {pay}
 
+# link: {link}\n-------------''')
 
-
-
-# #allows to open file
-# with open("https://www.jobstreet.com.ph/en/job-search/job-vacancy.php?specialization=186%2C191%2C192%2C102", 'r') as html_file:
-#     content = html_file.read()
-    
-#     soup = BeautifulSoup(content, 'lxml')
-#     job_title = soup.find_all('div', class_='sx2jih0 _2j8fZ_0 sIMFL_0 _1JtWu_0') #for specific tags; turns it into list
-    
-#     for job in job_title:         # prints each find from the list
-#         print(i.text)
